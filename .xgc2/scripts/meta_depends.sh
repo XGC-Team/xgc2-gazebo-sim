4d21b2eb167bf71fb33c5795f0eb08fb7e56811f
Lxk_buaa <867768510@qq.com>
2026-06-24 05:05:37 +0800

    Update Gazebo sim NMPC quickstart release

--- .xgc2/scripts/meta_depends.sh
+++ .xgc2/scripts/meta_depends.sh
@@ -34,7 +34,6 @@ packages=(
   ros-noetic-xgc2-gazebo-sim-px4-1-12
   ros-noetic-xgc2-gazebo-sim-px4-1-14
   ros-noetic-xgc2-gazebo-sim-fs150-sitl
-  ros-noetic-xgc2-gazebo-sim-cerlab-uav
 )
 
 depends=()
