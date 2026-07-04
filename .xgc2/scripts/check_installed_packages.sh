4bee3c50409bfe9be5f8516be273262ca7191315
Lxk_buaa <867768510@qq.com>
2026-07-04 23:21:15 +0800

    update gazebo sim scout dependency

--- .xgc2/scripts/check_installed_packages.sh
+++ .xgc2/scripts/check_installed_packages.sh
@@ -12,15 +12,16 @@ check_min_version() {
   dpkg --compare-versions "${installed_version}" ge "${min_version}"
 }
 
-check_min_version ros-noetic-xgc2-gazebo-sim-manager 1.0.45-1
-check_min_version ros-noetic-xgc2-gazebo-sim-examples 1.0.45-1
-check_min_version ros-noetic-xgc2-gazebo-sim-visualization 1.0.45-1
-check_min_version ros-noetic-xgc2-gazebo-sim-vrpn-bridge 1.0.44-1
-check_min_version ros-noetic-xgc2-gazebo-sim-worlds 1.0.21-1
-check_min_version ros-noetic-xgc2-gazebo-sim-scout 0.4.9-1
-check_min_version ros-noetic-xgc2-gazebo-sim-px4-1-12 1.12.3-7
-check_min_version ros-noetic-xgc2-gazebo-sim-px4-1-14 1.14.4-6
-check_min_version ros-noetic-xgc2-gazebo-sim-fs150-sitl 1.0.13-1
+check_min_version ros-noetic-xgc2-gazebo-sim-manager 1.1.0-12
+check_min_version ros-noetic-xgc2-gazebo-sim-examples 1.1.0-12
+check_min_version ros-noetic-xgc2-robot-visualization 0.1.0-1
+check_min_version ros-noetic-xgc2-gazebo-sim-visualization 1.1.0-6
+check_min_version ros-noetic-xgc2-gazebo-sim-vrpn-bridge 1.1.0-8
+check_min_version ros-noetic-xgc2-gazebo-sim-worlds 1.1.0-8
+check_min_version ros-noetic-xgc2-gazebo-sim-scout 0.4.9-6
+check_min_version ros-noetic-xgc2-gazebo-sim-px4-1-12 1.12.3-11
+check_min_version ros-noetic-xgc2-gazebo-sim-px4-1-14 1.14.4-10
+check_min_version ros-noetic-xgc2-gazebo-sim-fs150-sitl 1.1.0-7
 
 test -f "/opt/ros/noetic/share/gazebo_sim_worlds/worlds/empty/empty.world"
 test -f "/opt/ros/noetic/share/gazebo_sim_worlds/worlds/weston_robot_empty/weston_robot_empty.world"
