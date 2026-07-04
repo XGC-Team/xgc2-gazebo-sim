94a4907232c252dc19e98133606416e1a32d3a23
Lxk_buaa <867768510@qq.com>
2026-07-05 00:17:38 +0800

    update gazebo sim scout dependency

--- .xgc2/scripts/check_installed_packages.sh
+++ .xgc2/scripts/check_installed_packages.sh
@@ -18,13 +18,12 @@ check_min_version ros-noetic-xgc2-robot-visualization 0.1.0-1
 check_min_version ros-noetic-xgc2-gazebo-sim-visualization 1.1.0-7
 check_min_version ros-noetic-xgc2-gazebo-sim-vrpn-bridge 1.1.0-8
 check_min_version ros-noetic-xgc2-gazebo-sim-worlds 1.1.0-8
-check_min_version ros-noetic-xgc2-gazebo-sim-scout 0.4.9-7
+check_min_version ros-noetic-xgc2-gazebo-sim-scout 0.4.9-8
 check_min_version ros-noetic-xgc2-gazebo-sim-px4-1-12 1.12.3-11
 check_min_version ros-noetic-xgc2-gazebo-sim-px4-1-14 1.14.4-10
 check_min_version ros-noetic-xgc2-gazebo-sim-fs150-sitl 1.1.0-7
 
 test -f "/opt/ros/noetic/share/gazebo_sim_worlds/worlds/empty/empty.world"
-test -f "/opt/ros/noetic/share/gazebo_sim_worlds/worlds/weston_robot_empty/weston_robot_empty.world"
 test -f "/opt/ros/noetic/share/gazebo_sim_worlds/worlds/clearpath_playpen/clearpath_playpen.world"
 test -f "/opt/ros/noetic/share/gazebo_sim_worlds/worlds/corridor_dynamic_9/corridor_dynamic_9.world"
 test -f "/opt/ros/noetic/share/gazebo_sim_worlds/models/corridor/model.sdf"
